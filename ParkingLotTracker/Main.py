147638b2235e71a86d42b5f75303d0d46b5bf83d
maheshwari1357 <103213680+maheshwari1357@users.noreply.github.com>
2023-10-07 01:35:55 +0530

    Update

--- ParkingLotTracker/Main.py
+++ ParkingLotTracker/Main.py
@@ -1,5 +1,6 @@
 from ParkingLot import ParkingLot
 from VehicleNumberNotFoundException import VehicleNumberNotFoundException
+parkingLot=ParkingLot()
 while True:
 		print("Welcome to Parking Lot Tracker!!")
 		print("********************************")
@@ -11,21 +12,21 @@ while True:
 		if choice == 1:
 			try:
 				vehicleNumber = input("Please enter the vehicle number: ")
-				spotNumber = ParkingLot.retrieveParkingSpot(vehicleNumber)
-				level = ParkingLot.getLevel(spotNumber)
+				spotNumber = parkingLot.retrieveParkingSpot(vehicleNumber)
+				level = parkingLot.getLevel(spotNumber)
 				print("Spot No: ",spotNumber, " Level: ", level)
 				print(" ")
 			except VehicleNumberNotFoundException:
 				print("Exception Occured: Vehicle Number Invalid!!")
 		elif choice == 2:
 			vehicleNumber = input("Please enter the vehicle number: ")
-			spotNumber = ParkingLot.assignParkingSpot(vehicleNumber)
-			level = ParkingLot.getLevel(spotNumber)
+			spotNumber = parkingLot.assignParkingSpot(vehicleNumber)
+			level = parkingLot.getLevel(spotNumber)
 			print("Spot Assigned: " ,spotNumber," Level: ", level)
 			print(" ")
 		elif choice == 3:
 			password = input("Enter the admin password: ")
-			print(ParkingLot.getParkingMap(password))
+			print(parkingLot.getParkingMap(password))
 		elif choice == 4:
 			break
 		else:
--- ParkingLotTracker/ParkingLot.py
+++ ParkingLotTracker/ParkingLot.py
@@ -1,33 +1,33 @@
 from VehicleNumberNotFoundException import VehicleNumberNotFoundException
-parkingMap={}  #vehicleNumber as a Key and parkingSpot will be corresponding value 
-spotOccupied=[] #List of Spots already occupied.
-
 class ParkingLot:
-	def retrieveParkingSpot(vehicleNumber):
-		if vehicleNumber in parkingMap:
-			spotOccupied.remove(parkingMap[vehicleNumber])
-			tmp = parkingMap[vehicleNumber]
-			del parkingMap[vehicleNumber]
+	parkingMap={}  #vehicleNumber as a Key and parkingSpot will be corresponding value 
+	spotOccupied=[] #List of Spots already occupied.
+
+	def retrieveParkingSpot(self,vehicleNumber):
+		if vehicleNumber in self.parkingMap:
+			self.spotOccupied.remove(self.parkingMap[vehicleNumber])
+			tmp = self.parkingMap[vehicleNumber]
+			del self.parkingMap[vehicleNumber]
 			return tmp
 		else:
 			raise VehicleNumberNotFoundException
 
-	def assignParkingSpot(vehicleNumber):
-		if vehicleNumber in parkingMap:
+	def assignParkingSpot(self,vehicleNumber):
+		if vehicleNumber in self.parkingMap:
 			print("Vehicle number already exists!!!....with below parking details")
-			return parkingMap[vehicleNumber]
+			return self.parkingMap[vehicleNumber]
 		for i in range(1,41):
-			if i not in spotOccupied:
-				parkingMap[vehicleNumber] = i
-				spotOccupied.append(i)
+			if i not in self.spotOccupied:
+				self.parkingMap[vehicleNumber] = i
+				self.spotOccupied.append(i)
 				break
-		return parkingMap[vehicleNumber]
+		return self.parkingMap[vehicleNumber]
 
-	def getParkingMap(password):
+	def getParkingMap(self,password):
 		if password == "admin":
-			return parkingMap
+			return self.parkingMap
 		else:
 			return ("Incorrect Password")
 	
-	def getLevel(spotNumber):
+	def getLevel(self,spotNumber):
 		return ("A" if spotNumber<21 else "B")
